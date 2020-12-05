650985dfae52a281aa4c0f725e7bd1a282b4d9ba
marta <marta@DESKTOP-TMB7CRH.home>
2020-12-05 09:18:35 +0100

    Creación del método mover con los parámetros direccion y pasos.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.alfilajedrez;
 
+import javax.naming.OperationNotSupportedException;
+
 public class Alfil {
 
 	private Posicion posicion;
@@ -70,5 +72,74 @@ public class Alfil {
 	public Color getColor() {
 
 		return color;
+	}
+
+	public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
+
+		if (direccion == null) {
+
+			throw new NullPointerException("ERROR: La dirección no puede ser nula.");
+		}
+
+		if (pasos <= 0) {
+
+			throw new IllegalArgumentException("ERROR: El número de pasos debe ser positivo.");
+		}
+
+		switch (direccion) {
+
+		case ARRIBA_DERECHA:
+
+			try {
+
+				setPosicion(new Posicion(posicion.getFila() + pasos, (char) (posicion.getColumna() + pasos)));
+
+			} catch (IllegalArgumentException e) {
+
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			}
+
+			break;
+
+		case ABAJO_DERECHA:
+
+			try {
+
+				setPosicion(new Posicion(posicion.getFila() - pasos, (char) (posicion.getColumna() + pasos)));
+
+			} catch (IllegalArgumentException e) {
+
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			}
+
+			break;
+
+		case ABAJO_IZQUIERDA:
+
+			try {
+
+				setPosicion(new Posicion(posicion.getFila() - pasos, (char) (posicion.getColumna() - pasos)));
+
+			} catch (IllegalArgumentException e) {
+
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			}
+
+			break;
+
+		case ARRIBA_IZQUIERDA:
+
+			try {
+
+				setPosicion(new Posicion(posicion.getFila() + pasos, (char) (posicion.getColumna() - pasos)));
+
+			} catch (IllegalArgumentException e) {
+
+				throw new OperationNotSupportedException("ERROR: Movimiento no válido (se sale del tablero).");
+			}
+
+			break;
+
+		}
 	}
 }
