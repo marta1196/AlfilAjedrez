c214c69e2b0963fda67a1bbb36ef02821e6942a3
marta <marta@DESKTOP-TMB7CRH.home>
2020-12-05 08:58:50 +0100

    Creación de los métodos get y set para los atributos posicion y color.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -4,4 +4,29 @@ public class Alfil {
 
 	private Posicion posicion;
 	private Color color;
+
+	private void setPosicion(Posicion posicion) {
+
+		this.posicion = posicion;
+	}
+
+	public Posicion getPosicion() {
+
+		return posicion;
+	}
+
+	private void setColor(Color color) {
+
+		if (color == null || color.equals("")) {
+
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		}
+
+		this.color = color;
+	}
+
+	public Color getColor() {
+
+		return color;
+	}
 }
