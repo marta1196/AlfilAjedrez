9fd1bfd505e884386022c222fd5067fb88685286
marta <marta@DESKTOP-TMB7CRH.home>
2020-12-05 10:21:17 +0100

    Creación del método mostrarAlfil().

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -3,4 +3,17 @@ package org.iesalandalus.programacion.alfilajedrez;
 public class MainApp {
 
 	private static Alfil alfil;
+
+	private static void mostrarAlfil() {
+
+		try {
+
+			System.out.println(alfil.toString());
+
+		} catch (NullPointerException e) {
+
+			System.out.println("No se ha creado ningún alfil");
+		}
+
+	}
 }
