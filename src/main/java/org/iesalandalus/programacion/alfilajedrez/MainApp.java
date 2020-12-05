a3d28d41272b0cb9bad33c58fb45f2d02792425d
marta <marta@DESKTOP-TMB7CRH.home>
2020-12-05 10:22:48 +0100

    Creación del método mostrarMenu().

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -16,4 +16,14 @@ public class MainApp {
 		}
 
 	}
+
+	private static void mostrarMenu() {
+
+		System.out.println("Introduzca una opción:");
+		System.out.println("1.Crear alfil por defecto");
+		System.out.println("2.Crear un afil de un color");
+		System.out.println("3.Crear un afil de un color en una columna inicial (c o f)");
+		System.out.println("4.Mover el alfil");
+		System.out.println("5.Salir");
+	}
 }
