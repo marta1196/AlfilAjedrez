1d15a0dfc7ded64369faff7813dfc86fb3f4658c
marta <marta@DESKTOP-TMB7CRH.home>
2020-12-05 09:20:54 +0100

    Creación de los métodos equals y hashCode.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -142,4 +142,33 @@ public class Alfil {
 
 		}
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((color == null) ? 0 : color.hashCode());
+		result = prime * result + ((posicion == null) ? 0 : posicion.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Alfil other = (Alfil) obj;
+		if (color != other.color)
+			return false;
+		if (posicion == null) {
+			if (other.posicion != null)
+				return false;
+		} else if (!posicion.equals(other.posicion))
+			return false;
+		return true;
+	}
+
 }
